25c5e47daf47e3fb8c9dedecafdc5b80402b1bbc
vruiz-ru <161227190+vruiz-ru@users.noreply.github.com>
2025-03-11 14:15:20 +0100

    Update flood_fill.c

--- Exam_Rank_02/L4/flood_fill.c
+++ Exam_Rank_02/L4/flood_fill.c
@@ -93,7 +93,7 @@ typedef struct s_point
 	int		x;
 	int		y;
 }			t_point;
-
+/*
 void	fill(char **tab, t_point size, t_point matrix, char org, char to_fill)
 {
 	if (matrix.y < 0 || matrix.y >= size.y || matrix.x < 0 || matrix.x >= size.x
@@ -110,3 +110,74 @@ void	flood_fill(char **tab, t_point size, t_point begin)
 {
 	fill(tab, size, begin, tab[begin.y][begin.x], 'F');
 }
+*/
+
+void  flood_fill(char **tab, t_point size, t_point begin)
+{
+    char c;
+    t_point p;
+
+    c = tab[begin.y][begin.x];
+    tab[begin.y][begin.x] = 'F';
+    if (begin.y > 0 && tab[begin.y - 1][begin.x] == c)
+    {
+        p.x = begin.x;
+        p.y = begin.y - 1;
+        flood_fill(tab, size, p);
+    }
+    if ((begin.y < (size.y - 1)) && tab[begin.y + 1][begin.x] == c)
+    {
+        p.x = begin.x;
+        p.y = begin.y + 1;
+        flood_fill(tab, size, p);
+    }
+    if ((begin.x < (size.x - 1)) && tab[begin.y][begin.x + 1] == c)
+    {
+        p.x = begin.x + 1;
+        p.y = begin.y;
+        flood_fill(tab, size, p);
+    }
+    if (begin.x > 0 && tab[begin.y][begin.x - 1] == c)
+    {
+        p.x = begin.x - 1;
+        p.y = begin.y;
+        flood_fill(tab, size, p);
+    }
+}
+
+char** make_area(char** zone, t_point size)
+{
+	char** new;
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+	return (new);
+}
+
+int	main(void)
+{
+	t_point	size = {8, 5};
+	t_point	begin = {6, 1};
+
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001",
+	};
+	char**  area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	return (0);
+}
+
